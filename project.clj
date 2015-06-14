92b33e0d0f6ef07b9f68f90137f7e6c01859bca4
Chris Zheng <z@caudate.me>
2015-06-14 13:21:30 +1000

    developing 0.1.1

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject im.chit/gita "0.1.0"
+(defproject im.chit/gita "0.1.1-SNAPSHOT"
   :description "git on song"
   :url "https://www.github.com/zcaudate/gita"
   :license {:name "The MIT License"
             :url "http://http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [im.chit/hara.reflect "2.1.11"]
-                 [im.chit/hara.string.case "2.1.11"]
-                 [im.chit/vinyasa.maven "0.3.3"]
+                 [im.chit/hara.reflect "2.1.12"]
+                 [im.chit/hara.string.case "2.1.12"]
+                 [im.chit/vinyasa.maven "0.3.4"]
                  [org.eclipse.jgit "3.6.2.201501210735-r"]]
   :global-vars {*warn-on-reflection* true}
   :profiles {:dev {:dependencies [[midje "1.6.3"]]
